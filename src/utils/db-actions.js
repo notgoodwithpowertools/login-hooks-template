3cb66348cec7d21bdb388da578fbeb9f66943db4
notgoodwithpowertools <notgoodwithpowertools@gmail.com>
2020-03-21 09:57:13 +1100

    added db actions

--- src/utils/db-actions.js
+++ src/utils/db-actions.js
@@ -5,38 +5,32 @@ export var deleteFSDocId = (collection, id) => {
     console.log(`Deleting item at ${collection} with id: ${id}`);
     firestoreDB.collection(collection).doc(id).delete().then(() => {
         console.log("Document successfully deleted!");
+        // Go on to provide additional db changes if needed
         //   firestoreDB.collection("votes").doc(id).delete().then(() => {
         //     console.log("Votes deleted!");
         //   }).catch(function(error) {
         //     console.error("Error removing votes: ", error);
-        //   });
-
+        //   })
     }).catch(function (error) {
         console.error("Error removing document: ", error);
     });
 
 }
 
-
-
-
-
-// +++++++++++++++++++++++++++
-
 export var addItem = (dbPath, desc, comment, value, aDate) => {
 
     const dateStamp = new Date(aDate);
-    // console.log("Parsed datestamp:", dateStamp);
-
+    
     return firestoreDB.collection(dbPath)
         .add({
-            //   id: user.uid,
+
             desc: desc,
             comment: comment,
             value: value,
             date: dateStamp
 
         })
+        // moved the promise return to the component - Promises only resolve once.
         // .then(ref => {
         //     console.log('Added new document with ID: ', ref.id);
         //     // ref.update({datestamp: fieldValue.serverTimestamp()})
